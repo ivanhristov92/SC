32e63418cd68916320a4a872ed40e5cca6c61c7d
Ivan Hristov <ivan_hristov@mail.bg>
2017-10-13 19:09:22 +0300

    Update connectModule.js

--- client/utils/make-module/connectModule.js
+++ client/utils/make-module/connectModule.js
@@ -6,49 +6,57 @@ const path = require('path');
 const _ = require("ramda");
 const toRoot = path.join("..", "..");
 const config = require(path.join(toRoot, ".pm.config")).config;
+let foldNamingStrat = config.naming ? config.naming.moduleFolder : null;
+
 const fromArgs = require("./common").fromArgs;
 const {moduleName, moduleFolderName, fileName, cName, fName} = require("./naming");
 
-let modFoldName = moduleFolderName();
+let modFoldName = moduleFolderName(foldNamingStrat);
 let modName = moduleName();
 if(!modFoldName){throw new Error("Module Name is required")}
 
 
-let modEpicsPath =path.join(toRoot, config.modules, modFoldName, fName.epics(modFoldName));
-let modEpicsConst = cName.epics(modName);
 
 // // // // Epics
-let rEpFile = fs.readFileSync(config.rootEpic, "utf8");
-let split = rEpFile.split(/\/\/pm imports/)
-split.splice(1, 0, `\n//pm imports\nimport ${modEpicsConst} from "./${modEpicsPath}"\n`);
-let withImports = split.join(" ");
+{
+
+    let modEpicsPath =path.join(toRoot, config.modules, modFoldName, "exports");
+    let modEpicsConst = cName.epics(modName);
+
+    let rEpFile = fs.readFileSync(config.rootEpic, "utf8");
+    let split = rEpFile.split(/\/\/pm imports/)
+    split.splice(1, 0, `\n//pm imports\nimport {${modEpicsConst}} from "./${modEpicsPath}"\n`);
+    let withImports = split.join(" ");
 
 
-let split2 = withImports.split(/\/\/pm exports/);
-split2.splice(1, 0, `\n//pm exports\n...${modEpicsConst},\n`);
-let withExports = split2.join(" ");
+    let split2 = withImports.split(/\/\/pm exports/);
+    split2.splice(1, 0, `\n//pm exports\n...${modEpicsConst},\n`);
+    let withExports = split2.join(" ");
 
 
-let rootEpicPath = config.rootEpic;
-fs.writeFileSync(rootEpicPath, withExports);
+    let rootEpicPath = config.rootEpic;
+	fs.writeFileSync(rootEpicPath, withExports);
+}
 // // // // 
 
 
 // // // // Reducer
 {
+	let subStateName = fromArgs("subState", "subState");
 	let modReducerConst = cName.reducer(modName);
-	let modReducerPath = path.join(toRoot, config.modules, modFoldName, fName.reducer(modFoldName));
+	let modReducerPath = path.join(toRoot, config.modules, modFoldName, "exports");
 
 	let rRedFile = fs.readFileSync(config.rootReducer, "utf8");
 	let split = rRedFile.split(/\/\/pm imports/);
-	split.splice(1, 0, `\n//pm imports\nimport ${modReducerConst} from "./${modReducerPath}"\n`);
+	split.splice(1, 0, `\n//pm imports\nimport {${modReducerConst}} from "./${modReducerPath}"\n`);
 	let withImports = split.join(" ");
 
 
 	let split2 = withImports.split(/\/\/pm exports/);
-	split2.splice(1, 0, `\n//pm exports\nsubState: ${modReducerConst},\n`);
+
+	split2.splice(1, 0, `\n//pm exports\n${subStateName}: ${modReducerConst},\n`);
 	let withExports = split2.join(" ");
-	
+	console.log(withExports)
 	let rootReducerPath = config.rootReducer;
 	fs.writeFileSync(rootReducerPath, withExports);
 }
